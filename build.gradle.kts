9aa24e7681e050844415d0aba22241f613c36dc2
Martin Vysny <mavi@vaadin.com>
2021-04-09 12:53:44 +0300

    2.1-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 defaultTasks("clean", "build")
 
 group = "com.github.mvysny.dirutils"
-version = "2.0"
+version = "2.1-SNAPSHOT"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_7
